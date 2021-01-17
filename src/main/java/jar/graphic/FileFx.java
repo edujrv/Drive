b6f7ef8794b8a2491f39836c5e06ad9d24d841e2
edujrv <55923936+edujrv@users.noreply.github.com>
2021-01-17 12:18:45 -0300

    padding entre carpetas agregado, si seleccionas una carpeta se pone azul

--- src/main/java/jar/graphic/FileFx.java
+++ src/main/java/jar/graphic/FileFx.java
@@ -1,11 +1,14 @@
 package jar.graphic;
 
+import jar.controllers.Efectos;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.scene.image.Image;
 import javafx.scene.control.Label;
@@ -13,18 +16,20 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import java.awt.*;
+import javafx.event.Event;
 
-public class FileFx extends Pane{
 
+
+public class FileFx extends VBox {
+    private FileFx prevFile = null;
+    private Label title = new Label();
    // @FXML
     public FileFx(){
-        System.out.println("GSDAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-
         this.setPrefSize(200,175);
         try {
             Image pic = new Image("jar/images/logoDrive.png");
-            Label title = new Label("HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-
+            setTitle("HOLAAAAAAAAAAAAAAAAAA");
+            changeTitleColor(0,0,0);
 
             ImageView icon = new ImageView(pic);
             icon.setFitWidth(200);
@@ -33,23 +38,75 @@ public class FileFx extends Pane{
             title.relocate(0,155);
             title.setMaxWidth(200);
             title.setStyle("-fx-font-size: 18; -fx-font: Normal 18 'Agency FB'");
-            this.setPadding(new javafx.geometry.Insets(0,0,0,1000));
-            //this.snapSpaceX(100000);
-            this.setStyle("-fx-border-color: #bababa; " +
-                    "-fx-border-width: 1;" +
-                    "-fx-border-radius: 10");
+            title.setMinSize(200,25);
+
             this.getChildren().addAll(icon,title);
+
+            this.setStyle("-fx-border-color: #bababa; " +
+                            "-fx-border-width: 1;" +
+                            "-fx-border-radius: 10; " +
+                            "-fx-padding: 20; " +
+                            "-fx-border-insets: 10 5 0 5");
+
+             this.setOnMouseClicked(this::fileSelected);
+
+
 
         }catch (Exception e){
             System.out.println(e);
         }
 
+    }
 
+    public void setTitle(String title) {
+        this.title.setText(title);
 
+    }
+    public void changeTitleColor(int red, int green, int blue){
+        this.title.setTextFill(Color.rgb(red,green,blue));
+    }
+    public void changeTitleBackground(int red, int green, int blue){
 
+        this.title.setStyle("-fx-background-color: "+red +" "+green+" "+blue);
+    }
+    public void changeTitleBackground(){
 
+        this.title.setStyle("-fx-background-color: #099194; "+
+                            "-fx-font-size: 18; " +
+                            "-fx-font: Normal 18 'Agency FB'");
+      /*  this.setStyle("-fx-border-color: #099194; " +
+                "-fx-background-color: #80e0e2;"+
+                "-fx-border-width: 1;" +
+                "-fx-border-radius: 10; " +
+                "-fx-padding: 20; " +
+                "-fx-border-insets: 10 5 0 5");
+                */
 
+    }
 
+    @FXML
+    public void fileSelected (Event e) {
+        FileFx actualFile = (FileFx) e.getSource();
+        String btnName = actualFile.getId();
 
+        if(prevFile != null){
+            prevFile.changeTitleColor(0,0,0);
+            prevFile.setStyle("-fx-border-color: #000000; " +
+                    "-fx-border-width: 1;" +
+                    "-fx-border-radius: 10; " +
+                    "-fx-padding: 20; " +
+                    "-fx-border-insets: 10 5 0 5");
+        }
+        prevFile = actualFile;
+        prevFile.setId(actualFile.getId());
+
+        //actualFile.changeTitleColor(9,145,148);
+        actualFile.setEffect(Efectos.blueOn());
+        actualFile.setStyle("-fx-border-color: #099194; " +
+                "-fx-border-width: 1;" +
+                "-fx-border-radius: 10; " +
+                "-fx-padding: 20; " +
+                "-fx-border-insets: 10 5 0 5");
+        actualFile.changeTitleBackground();
     }
 }
