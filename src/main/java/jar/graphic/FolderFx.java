73190b90e4012c5d83c7e849fd36176742755c2e
Mateo Pagnucco <mateopagnucco@gmail.com>
2021-03-28 18:52:31 -0300

    Pasado de los estilos de FolderFx al style.css

--- src/main/java/jar/graphic/FolderFx.java
+++ src/main/java/jar/graphic/FolderFx.java
@@ -19,22 +19,15 @@ public class FolderFx extends HBox implements ISelectable {
         private Label label = new Label("");
 
         public FolderFx(FolderDTO folder, HomeController hcontroller) {
+                unselect();
 
                 label.setText(folder.getName());
-                label.setMaxWidth(130);
-                label.setStyle("-fx-font-size: 18;" + " -fx-font: Normal 18 'Agency FB';" + " -fx-padding: 0 0 0 25;"
-                                + " -fx-text-fill: #3e3e3e");
 
                 ImageView imageView = new ImageView(new Image("/jar/images/folderRed.png"));
                 imageView.setFitWidth(25);
                 imageView.setFitHeight(25);
 
                 getChildren().addAll(imageView, label);
-
-                setPrefSize(300, 75);
-                setStyle("-fx-border-color: #bababa; " + "-fx-border-width: 1;" + "-fx-border-radius: 10; "
-                                + "-fx-padding: 0 0 0 25; " + "-fx-border-insets: 15 0 0 20");
-                setAlignment(Pos.CENTER_LEFT);
 
                 setOnMouseClicked(new EventHandler<MouseEvent>() {
                         @Override
@@ -58,29 +51,40 @@ public class FolderFx extends HBox implements ISelectable {
 
         @Override
         public void select() {
-                label.setStyle("-fx-font-size: 18; " + "-fx-font: Normal 18 'Agency FB';" + " -fx-padding: 0 0 0 25;"
-                                + "-fx-text-fill: #0b7bf3");
+                label.getStyleClass().setAll("folder-button__label", "folder-button__label--selected");
+                getStyleClass().setAll("center-pane__folder-button", "center-pane__folder-button--selected");
 
-                this.setStyle("-fx-border-color: #bababa; " + " -fx-background-insets: 15 0 0 20;"
-                                + " -fx-background-radius: 10;" + " -fx-background-color: #c4d5ff;"
-                                + "-fx-border-width: 1;" + "-fx-border-radius: 10; " + "-fx-padding: 0 0 0 25; "
-                                + "-fx-border-insets: 15 0 0 20;");
+                // label.setStyle("-fx-font-size: 18; " + "-fx-font: Normal 18 'Agency FB';" + "
+                // -fx-padding: 0 0 0 25;"
+                // + "-fx-text-fill: #0b7bf3");
 
-                this.setAlignment(Pos.CENTER_LEFT);
-                this.setPrefSize(300, 75);
+                // this.setStyle("-fx-border-color: #bababa; " + " -fx-background-insets: 15 0 0
+                // 20;"
+                // + " -fx-background-radius: 10;" + " -fx-background-color: #c4d5ff;"
+                // + "-fx-border-width: 1;" + "-fx-border-radius: 10; " + "-fx-padding: 0 0 0
+                // 25; "
+                // + "-fx-border-insets: 15 0 0 20;");
+
+                // this.setAlignment(Pos.CENTER_LEFT);
+                // this.setPrefSize(300, 75);
         }
 
         @Override
         public void unselect() {
-                label.setStyle("-fx-font-size: 18;" + " -fx-font: Normal 18 'Agency FB';" + " -fx-padding: 0 0 0 25;"
-                                + " -fx-text-fill: #3e3e3e");
+                label.getStyleClass().setAll("folder-button__label", "folder-button__label--unselected");
+                getStyleClass().setAll("center-pane__folder-button", "center-pane__folder-button--unselected");
 
-                this.setStyle("-fx-border-color: #bababa; " + "-fx-border-width: 1;" + "-fx-border-radius: 10; "
-                                + "-fx-padding: 0 0 0 25; " + "-fx-border-insets: 15 0 0 20;"
-                                + " -fx-background-color: transparent");
+                // label.setStyle("-fx-font-size: 18;" + " -fx-font: Normal 18 'Agency FB';" + "
+                // -fx-padding: 0 0 0 25;"
+                // + " -fx-text-fill: #3e3e3e");
 
-                this.setAlignment(Pos.CENTER_LEFT);
-                this.setPrefSize(300, 75);
+                // this.setStyle("-fx-border-color: #bababa; " + "-fx-border-width: 1;" +
+                // "-fx-border-radius: 10; "
+                // + "-fx-padding: 0 0 0 25; " + "-fx-border-insets: 15 0 0 20;"
+                // + " -fx-background-color: transparent");
+
+                // this.setAlignment(Pos.CENTER_LEFT);
+                // this.setPrefSize(300, 75);
         }
 
 }
